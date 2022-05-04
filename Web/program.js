59e54a625c60d33800f9fae144f838181f974228
Ca-Len-Men <88229844+Ca-Len-Men@users.noreply.github.com>
2022-05-04 21:05:08 +0700

    Update program.js

--- Web/program.js
+++ Web/program.js
@@ -10,3 +10,17 @@ function increaseScaleX(id, time){
     doc.style.transform = 'scaleX(100%)';
     doc.style.transition = time;
 }
+
+
+
+function setEditor(id, wid, hei){
+    var editor = ace.edit(id);
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/c_cpp");
+    editor.setReadOnly(true);
+    var doc = document.getElementById(id);
+    doc.style.fontSize='18px';
+    doc.style.width = wid;
+    doc.style.height = hei;
+    doc.style.marginBottom = '10px';
+}
