dd8d3d3b054c9b8abddcf9eb585265d75a45bf15
Ca-Len-Men <88229844+Ca-Len-Men@users.noreply.github.com>
2022-07-30 21:35:28 +0700

    Add files via upload

--- DSA Document/JS/BoxPlayer.js
+++ DSA Document/JS/BoxPlayer.js
@@ -1,37 +1,64 @@
 class BoxPlayer{
-    constructor(btn_show, box_show, show){
-        this.btn_show = btn_show
-        this.box_show = box_show
+    constructor(btn, box, show){
+        this.btn = btn
+        this.box = box
         this.show = show
     }
 
     action(){
-        if(this.show){
-            this.box_show.style.display = 'none'
-            this.btn_show.style.transform = 'rotate(90deg)'
-        }else{
-            this.box_show.style.display = 'block'
-            this.btn_show.style.transform = 'rotate(0)'
-        }
         this.show = !this.show
+        if(this.show){
+            this.box.classList.remove('hidden')
+            this.box.classList.remove('box-animation-hidden')
+            this.box.classList.add('box-animation-show')
+            this.btn.style.transform = 'rotate(90deg)'
+        }else{
+            this.box.classList.add('hidden')
+            this.box.classList.remove('box-animation-show')
+            this.box.classList.add('box-animation-hidden')
+            this.btn.style.transform = 'rotate(0)'
+        }
     }
 }
 
+
+var check = true
+
+const list_btn_show = document.querySelectorAll('.button-show')
+const list_box_show = document.querySelectorAll('.box-show')
+var listBoxPlayer = []
+
+var page_index = 0
+const list_page = document.querySelectorAll('.page')
+const list_card = document.querySelectorAll('.mainpage > .card')
+const list_back_page = document.querySelectorAll('.back-page')
+
 function init_function(){
-    const len = 1
-    const btn_show = '.button-show'
-    const box_show = '.box-show'
-    list_btn_show = document.querySelectorAll(btn_show)
-    list_box_show = document.querySelectorAll(box_show)
-    list_BoxPlayer = []
-    
-    for(var i = 0; i < list_box_show.length; ++i){
-        box_player = new BoxPlayer(list_btn_show[i], list_box_show[i], false)
-        list_BoxPlayer.push(box_player)
-        box_player.btn_show.addEventListener('click', function(){
-            box_player.action()
+    for(let i = 0; i < list_btn_show.length; ++i){
+        listBoxPlayer.push(new BoxPlayer(list_btn_show[i], list_box_show[i], false))
+        list_btn_show[i].addEventListener('click', function() {
+            listBoxPlayer[i].action()
+            check = false
         })
     }
+
+    for(let i = 0; i < list_card.length; ++i)
+        list_card[i].addEventListener('click', function(){
+            if(check){
+                list_page[page_index].classList.add('hidden')
+                page_index = i + 1
+                list_page[page_index].classList.remove('hidden')
+            }else
+                check = true
+        })
+
+    for(let i = 0; i < list_back_page.length; ++i)
+        list_back_page[i].addEventListener('click', function(){
+            check = true
+            list_page[page_index].classList.add('hidden')
+            page_index = 0
+            list_page[0].classList.remove('hidden')
+        })
 }
 
 document.addEventListener('DOMContentLoaded', init_function);
